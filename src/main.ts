cab364f3348b0f904772e3cb552ed178e48a3037
pablo <pablogabriel.maruottolo@cieep.com>
2024-04-26 19:04:38 +0200

    primer second

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  /*
+ /* 
   app.enableCors({
     origin: 'http://localhost:8100',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  });*/
+  });
+*/
 
+  // Habilitar CORS para permitir solicitudes de cualquier origen
+  app.enableCors({
+    origin: true, // Permite cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permite el envío de cookies y headers de autenticación
+    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
+  });
+  
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
